4217f7227195ee45ae1b1a30a4b48711ff1a2264
uechi <try.rhtme.ddr@gmail.com>
2020-03-21 18:09:42 +0900

    403,500例外処理作成-production mode check

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,2 +1,9 @@
 class TopController < ApplicationController
+    def index
+        # raise
+        raise Forbidden
+        # raise IpAddressRejected
+        # productionモードで起動することで、例外処理を起こせる。
+        render action "index"
+    end
 end
