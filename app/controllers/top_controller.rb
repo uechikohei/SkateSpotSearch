5aa411087373782976b30ea6ae4e0483ba70472b
uechi <try.rhtme.ddr@gmail.com>
2020-03-21 18:13:22 +0900

    raise コメントアウト

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
     def index
         # raise
-        raise Forbidden
+        # raise Forbidden
         # raise IpAddressRejected
         # productionモードで起動することで、例外処理を起こせる。
         render action "index"
