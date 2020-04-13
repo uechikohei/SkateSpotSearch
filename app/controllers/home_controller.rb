0e36b9300ea35876406ecdf0d9e90f06f84a62fd
uechi <try.rhtme.ddr@gmail.com>
2020-04-13 10:09:43 +0900

    stock_quote uninstall

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-      @api = StockQuote::Stock.new(api_key: 'pk_d9d2be72b48e42d2bbca152beb28d99a')
-
-    begin
-    if params[:ticker] == ""
-      @nothing = "検索したい銘柄を入力してください。"
-    elsif params[:ticker]
-      @stock = StockQuote::Stock.quote(params[:ticker])
-    end
-    rescue RuntimeError => e
-      @error = "該当する銘柄を見つけることはできませんでした。"
-    end
-
   end
 
 end
