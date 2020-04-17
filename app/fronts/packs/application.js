b227e6ccbe0d5ef8492fb5f3c613fc24fadc8dfa
uechi <try.rhtme.ddr@gmail.com>
2020-04-18 00:18:51 +0900

    viewsページに、yarn使用してjquery適用

--- app/fronts/packs/application.js
+++ app/fronts/packs/application.js
@@ -2,8 +2,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+global.$ = require('jquery')
 // require("bootstrap"); // bootstrap_custom.js を使います
 
 import "bootstrap"
-import '../src/stylesheets/application'
+
 import "@fortawesome/fontawesome-free/js/all"
+
+
+//scss
+import '../src/stylesheets/application'
+//Javascript
+import '../src/javascripts/application';
---
+++ app/fronts/src/javascripts/application.js
@@ -0,0 +1,4 @@
+$(function() {
+    $(".title").css("color", "red");
+
+});
