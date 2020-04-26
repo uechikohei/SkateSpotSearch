82c776d676bbddbc98c5573de1f59f1fe3e464f3
uechi <try.rhtme.ddr@gmail.com>
2020-04-26 21:39:05 +0900

    #8 簡単ログイン機能再追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
 
   mount_uploader :image, ImageUploader
   validates :name, presence: true, length: { maximum: 80}
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com', name: 'guest') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
 end
