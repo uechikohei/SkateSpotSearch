bd09eebe5bca5e4f3d65a8b3616202fc9a221876
uechi <try.rhtme.ddr@gmail.com>
2020-04-26 21:12:31 +0900

    簡単ログイン追加 sessions controller使用

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   get 'users/:id', to: 'users#show'
   post "/" => 'home#index'
   resources :maps
