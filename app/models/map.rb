9288ba0fc01450bd6c38bcf60be9c255763f5298
uechi <try.rhtme.ddr@gmail.com>
2020-05-04 21:45:06 +0900

    maps#new ページ修正

--- app/models/map.rb
+++ app/models/map.rb
@@ -2,6 +2,8 @@ class Map < ApplicationRecord
   mount_uploader :picture, PictureUploader
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
+  validates :title, length: { maximum: 20}
+  validates :content, length: { maximum: 300}
 
   def self.search(address)
     return Map.all unless address
