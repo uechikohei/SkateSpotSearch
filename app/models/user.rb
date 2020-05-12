8050d8b2c038c8cf574cf4d1123d70695f2de864
uechi <try.rhtme.ddr@gmail.com>
2020-05-13 00:58:01 +0900

    views修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
+  has_many :maps, dependent: :destroy
+
   mount_uploader :image, ImageUploader
   validates :name, presence: true, length: { maximum: 80}
 
