58ac649304547670d74e03a807224f33fafb8715
uechi <try.rhtme.ddr@gmail.com>
2020-05-15 19:59:57 +0900

    #15 relationshipモデル作成。マイグレーションまで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_161454) do
+ActiveRecord::Schema.define(version: 2020_05_15_105012) do
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "map_id", null: false
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_05_12_161454) do
     t.string "title"
     t.boolean "helmet", default: false, null: false
     t.boolean "spot_style", default: true
+  end
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
