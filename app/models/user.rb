564c9a561aba8b550ef0763614532f33a3df308f
uechi <try.rhtme.ddr@gmail.com>
2020-05-15 20:03:34 +0900

    #15 フォロー、アンフォロー、フォローチェックなどメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,21 @@ class User < ApplicationRecord
   has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
   has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
 
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
+
   # 最近作成されたユーザーから表示
   default_scope -> { order(created_at: :desc) }
   # 一度に表示する投稿数
