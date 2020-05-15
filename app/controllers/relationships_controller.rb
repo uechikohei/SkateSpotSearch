e23abea3886caf5c0159dfb08e7d3a6b390770e9
uechi <try.rhtme.ddr@gmail.com>
2020-05-15 20:06:15 +0900

    #15 relationshipsコントローラー作成。follow,unfollowアクションを追加

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,11 @@
+class RelationshipsController < ApplicationController
+  def follow
+    current_user.follow(params[:id])
+    redirect_to root_path
+  end
+
+  def unfollow
+    current_user.unfollow(params[:id])
+    redirect_to root_path
+  end
+end
