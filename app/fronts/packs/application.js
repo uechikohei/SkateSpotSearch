743e0bdf3313aee32ed0a3581403f3fe204b39e4
uechi <try.rhtme.ddr@gmail.com>
2020-05-24 17:59:53 +0900

    bootstrap class 除去

--- app/fronts/packs/application.js
+++ app/fronts/packs/application.js
@@ -1,10 +1,13 @@
 require("@rails/activestorage").start()
 require.context('../images', true)
 
+
+
 //プラグイン
 import "@fortawesome/fontawesome-free/js/all"
 
 //scss
 import '../src/stylesheets/application'
+
 //Javascript
 import '../src/javascripts/application'
--- db/schema.rb
+++ db/schema.rb
@@ -22,15 +22,15 @@ ActiveRecord::Schema.define(version: 2020_05_15_105012) do
   end
 
   create_table "maps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "picture"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "picture"
+    t.text "content"
+    t.string "title"
     t.boolean "helmet", default: false, null: false
     t.boolean "spot_style", default: true
   end
@@ -43,8 +43,6 @@ ActiveRecord::Schema.define(version: 2020_05_15_105012) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -52,6 +50,8 @@ ActiveRecord::Schema.define(version: 2020_05_15_105012) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
