a6173378d4e89e00f00272317870d171b2575749
uechi <try.rhtme.ddr@gmail.com>
2020-06-10 21:30:41 +0900

    serach button レイアウト変更とmobile版ハンバーガーメニュー作成

--- app/fronts/packs/application.js
+++ app/fronts/packs/application.js
@@ -1,15 +1,15 @@
-
 require("@rails/activestorage").start()
+
 require.context('../images', true)
+
 global.$ = require('jquery')
+
 import Rails from '@rails/ujs';
 Rails.start();
 
 //プラグイン
 import "@fortawesome/fontawesome-free/js/all"
-
 //scss
 import '../src/stylesheets/application'
 
-//Javascript
-import '../src/javascripts/application'
+
--- app/fronts/packs/google.js
+++ app/fronts/packs/google.js
@@ -1,3 +1,3 @@
-global.$ = require('jquery')
+
 import "../src/javascripts/google/routesmap.js";
 import "../src/javascripts/google/postmap.js";
