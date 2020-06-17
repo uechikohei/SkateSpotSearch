d311204b8d4c0e8ca0b2b0fb620c0fd2cb5fd61e
uechi <try.rhtme.ddr@gmail.com>
2020-06-18 01:14:21 +0900

    いいねボタンajax化、JSリクエストで送信する

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,18 +2,12 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.create(map_id: params[:map_id])
-    respond_to do |format|
-      if @like.save
-        format.html { redirect_back(fallback_location: root_path) }
-        format.js
-      end
-    end
+    @like.save
   end
 
   def destroy
     @like = Like.find_by(map_id: params[:map_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
   end
 
 end
