870e3a2cc43aae56fbd603478a6f8ef7a0882b78
uechi <try.rhtme.ddr@gmail.com>
2020-06-29 11:55:38 +0900

    #25 user_infoカード内の既存フォローボタンをajaxに変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,25 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user
 
   def follow
     current_user.follow(params[:id])
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 
   def unfollow
     current_user.unfollow(params[:id])
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
 end
