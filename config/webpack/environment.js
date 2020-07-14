5b45c4f792359d6124db7823b94ad4ce2f665689
uechi <try.rhtme.ddr@gmail.com>
2020-07-14 16:46:38 +0900

    docker-composeを本番環境と開発環境とで分ける

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-// resolve-url-loader must be used before sass-loader
-environment.loaders.get('sass').use.splice(-1, 0, {
-    loader: 'resolve-url-loader',
-});
-
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-      $:  "jquery/src/jquery",
-      jQuery:"jquery/src/jquery"
-    })
-)
-
 module.exports = environment
-
