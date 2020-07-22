800078499f0aaae11d293ff763895208413178ee
uechi <try.rhtme.ddr@gmail.com>
2020-07-22 17:01:26 +0900

    [rubocop]-a レイアウト/インデントの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,17 +12,17 @@ class ApplicationController < ActionController::Base
       @search_feeds = @user_key.result(distinct: true).page(params[:page])
     end
 
-    protected
+  protected
 
     def after_sign_up_path_for(resource)
-        edit_user_registration_path
+      edit_user_registration_path
     end
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up,        keys: [:name])
-        devise_parameter_sanitizer.permit(:sign_up,        keys: [:image])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:image])
+      devise_parameter_sanitizer.permit(:sign_up,        keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up,        keys: [:image])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:image])
     end
     # エラークラスを定義する
     class Forbidden         < ActionController::ActionControllerError; end
@@ -38,16 +38,16 @@ class ApplicationController < ActionController::Base
 
     # Forbiddenエラーがあったときに呼び出されるメソッド
     private def rescue403(e)
-        @exception = e
-        logger.error "error_404: #{e.message}"
-        logger.error e.backtrace.join("\n")
-        render "errors/forbidden", status: 403
+      @exception = e
+      logger.error "error_404: #{e.message}"
+      logger.error e.backtrace.join("\n")
+      render "errors/forbidden", status: 403
     end
     # StandartErrorが会った場合、rescue500メソッドで呼び出される内容
     private def rescue500(e)
-        # viewsのerrors/interval_server_errorページを返すよ
-        logger.error "error_404: #{e.message}"
-        logger.error e.backtrace.join("\n")
-        render "errors/internal_server_error", status: 500
+      # viewsのerrors/interval_server_errorページを返すよ
+      logger.error "error_404: #{e.message}"
+      logger.error e.backtrace.join("\n")
+      render "errors/internal_server_error", status: 500
     end
 end
