f81ce8658d3005ff9d77c4edad2f22daf5f7d5a3
uechi <try.rhtme.ddr@gmail.com>
2020-07-22 10:07:57 +0900

    [add]見やすいレイアウトに調整。gitagnore整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: %i[index show destroy edit]
+  before_action :authenticate_user!, only: %i[destroy edit]
 
   def index
     @users = User.all
