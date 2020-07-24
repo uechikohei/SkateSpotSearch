d58f8d5ed57e7794aaa04f9ec7fbb93501f2bd63
uechi <try.rhtme.ddr@gmail.com>
2020-07-24 10:19:20 +0900

    [rubocop] ガード節を使用

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,9 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[destroy]
 
   def check_guest
-    if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
-    end
+    redirect_to root_path, alert: 'ゲストユーザーは削除できません。' if resource.email == 'guest@example.com'
   end
 
   def destroy; end
