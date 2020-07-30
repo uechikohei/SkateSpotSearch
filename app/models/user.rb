ad3941136efa670f7b5cf5c6d64e49c1f7124ba9
uechi <try.rhtme.ddr@gmail.com>
2020-07-30 17:58:12 +0900

    [circleci] modeal/user.rb uniqueness: trueに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
 
   mount_uploader :image, ImageUploader
-  validates :name,  presence: true, uniqueness: true,
+  validates :name,  presence: true, uniqueness: { case_sensitive: false },
                     length: { minimum: 2, maximum: 20 }
 
   validates :email, presence: true, uniqueness: { case_sensitive: true },
