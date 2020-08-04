7e9a99f4bc125b7d49dc8f557d0b7fd252014d31
uechi <try.rhtme.ddr@gmail.com>
2020-08-04 14:51:50 +0900

    nginxをソケット通信に変更してECSへデプロイ。&環境変数設定。&dotenv追加

--- config/puma.rb
+++ config/puma.rb
@@ -5,3 +5,5 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
