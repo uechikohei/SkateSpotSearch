d95bb6767969d10ccc5a5d8e576fe7f6b22ddebd
uechi <try.rhtme.ddr@gmail.com>
2020-08-06 17:42:40 +0900

    サイト説明文追加＆ログインボタン追加

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,10 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
-port        ENV.fetch("PORT") { 3000 }
+#port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
+#追加でecsのec2に配置したappコンテナが起動を続ける
 app_root = File.expand_path("../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
