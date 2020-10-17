9dd29899444e9b378a72c5962b576d6c6ce98c21
uechi <try.rhtme.ddr@gmail.com>
2020-10-17 10:50:15 +0900

    googlemaps api key をcredentialsに保存して使用

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,6 @@
 Geocoder.configure(
   lookup: :google,
   language: :ja,
-  api_key: "AIzaSyA5qtdZ0_IRKciy41eDwLL9fTQzwR_7BAI",
+  api_key: Rails.application.credentials.google_map_api[:key],
   units: :km
 )
